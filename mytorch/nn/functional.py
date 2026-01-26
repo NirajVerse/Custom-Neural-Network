fixed softmax fn error of data

mytorch/nn/functional.py
@@ -27,15 +27,15 @@ def relu(x: Tensor):  ## implementing relu activation function:
 def softmax(x: Tensor, dim: int = -1) -> Tensor:
             ### BEGIN SOLUTION
         # Numerical stability: subtract max to prevent overflow
-        x_max_data = np.max(x.data, axis=dim, keepdims=True)
+        x_max_data = np.max(x._data, axis=dim, keepdims=True)
         x_max = Tensor(x_max_data)
         x_shifted = x - x_max  # Tensor subtraction
 
         # Compute exponentials
-        exp_values = Tensor(np.exp(x_shifted.data))
+        exp_values = Tensor(np.exp(x_shifted._data))
 
         # Sum along dimension
-        exp_sum_data = np.sum(exp_values.data, axis=dim, keepdims=True)
+        exp_sum_data = np.sum(exp_values._data, axis=dim, keepdims=True)
         exp_sum = Tensor(exp_sum_data)
 
         # Normalize to get probabilities